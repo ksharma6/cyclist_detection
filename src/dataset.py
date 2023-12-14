170be9e8f16b11c1bd759aee817a36c2356b462d
ksharma6 <ksharma6@tutanota.com>
2023-12-13 19:54:31 -0800

    Added ImageDataset class

--- src/dataset.py
+++ src/dataset.py
@@ -1,23 +1,39 @@
+import os
+from os.path import isfile, join
+import re
 
+import torchvision
 
 class ImageDataset:
-    def __init__(self, root):
+    def __init__(self, root, transforms = None):
         self.root = root
-        #get image paths
-        #get paths to text file
+        self.images = get_paths(root + "images/")
+        
+        self.bboxes = get_paths(root + "labels/")
+        self.transforms = transforms
 
     def __len__(self):
         #return # of images in object
-        pass
+        return len(self.images)
 
     def __getitem__(self, idx):
+        image = torchvision.io.read_image(self.images[idx], mode = torchvision.io.ImageReadMode.RGB)
         #read image path
-        #read text file for bbox
+        bbox = []
+        with open(self.bboxes[idx]) as file:
+            for line in file:
+                bbox.append(line)
+        bbox = [s.strip('\n') for s in bbox]
 
-
-        #return {image: tensor, bbox: box dimensions}
-            #how does this work with variable # of objects in iamge?
-        pass
-
+        sample = {}
+        sample['id'] = re.sub(r'(^0+)|(.txt)', "", self.bboxes[idx])
+        sample['image'] = image
+        sample['bboxes'] = bbox
+        return sample
 
 #helper functions
+def get_paths(directory): 
+    paths = [(directory + f) for f in os.listdir(directory) if isfile(join(directory, f))]
+    return sorted(paths)
+
+
